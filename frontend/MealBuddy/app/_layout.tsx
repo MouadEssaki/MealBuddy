8647c390191b67ce0766c33937ab4a510e3a4474
Dabachi <e2290266@cmaisonneuve.qc.ca>
2025-02-05 13:06:43 -0500

    premier routage

--- frontend/MealBuddy/app/_layout.tsx
+++ frontend/MealBuddy/app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+// app/_layout.js
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'; // For icons (optional)
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Tabs>
+      {/* Home Tab */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
